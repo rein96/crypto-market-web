7b55e1209ed38070ab5cc71cde364a4ceb417ef9
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-08-31 23:39:48 +0700

    feat: add endpoints file

---
+++ networks/endpoints.ts
@@ -0,0 +1,7 @@
+const endpoints = {
+  tradePriceChanges: 'https://api.pintu.co.id/v2/trade/price-changes',
+  walletSupportedCurrencies:
+    'https://api.pintu.co.id/v2/wallet/supportedCurrencies',
+};
+
+export default endpoints;
